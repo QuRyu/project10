248538b2aa5383298fc4e48498670b5c42606ff7
QuRyu <Quentin.Liu.0415@gmail.com>
2017-12-05 22:45:03 -0500

    add project

---
+++ src/main/java/CoreInstruction.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Quentin on 12/5/17.
+ */
+public class CoreInstruction {
+}
---
+++ src/main/java/Instruction.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Quentin on 12/5/17.
+ */
+public interface Instruction {
+    ArrayList<CoreInstruction> generate();
+}
+
