4218b52d4c3a9521ac4e293852c18e4d79696425
QuRyu <Quentin.Liu.0415@gmail.com>
2017-12-07 19:56:54 -0500

    delete line number field

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -7,19 +7,15 @@ public class Add implements Instruction {
     private boolean isIndirect;
     private String opA;
     private String opB;
-    private int n; // line number
 
-    public Add(String opA, String opB, int n, boolean isIndirect) throws Exception {
-        if (!Register.tableE.contains(opA) || !Register.tableB.contains(opB))
-            throw new Exception("wrong operand " + (Register.tableE.contains(opA) ? opB : opA) + "at line " + n);
+    public Add(String opA, String opB, boolean isIndirect) throws Exception {
         this.opA = opA;
         this.opB = opB;
-        this.n = n;
         isIndirect = isIndirect;
     }
 
-    public Add(String opA, String opB, int n) throws Exception {
-        this(opA, opB, n,false);
+    public Add(String opA, String opB) throws Exception {
+        this(opA, opB, false);
     }
 
     public ArrayList<CoreInstruction> generate() {
