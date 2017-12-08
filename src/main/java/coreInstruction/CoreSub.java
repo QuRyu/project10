4a7f8795c3102138fd0b3555ca5678af82a1b166
QuRyu <Quentin.Liu.0415@gmail.com>
2017-12-08 18:11:00 -0500

    expand the functionality of coresub

--- src/main/java/coreInstruction/CoreSub.java
+++ src/main/java/coreInstruction/CoreSub.java
@@ -2,6 +2,7 @@ package coreInstruction;
 
 import instruction_type.Addressing_mode;
 import instruction_type.Operand;
+import instruction_type.PCAddr;
 import subneg.Subneg;
 
 import java.util.ArrayList;
@@ -11,14 +12,25 @@ import java.util.ArrayList;
  */
 public class CoreSub extends CoreInstruction {
 
+    private boolean conditional; // if branch if the result is less than 0
+    private PCAddr jumpToAddr;
+
     public CoreSub(Operand src, Addressing_mode src_mode, Operand dst, Addressing_mode dst_mode) {
         super(src, src_mode, dst, dst_mode);
+        conditional = false;
     }
 
+    public CoreSub(Operand src, Addressing_mode src_mode, Operand dst, Addressing_mode dst_mode, PCAddr jumpToAddr) {
+        super(src, src_mode, dst, dst_mode);
+        this.jumpToAddr = jumpToAddr;
+        conditional = true;
+    }
 
     public ArrayList<Subneg> generate() {
         ArrayList<Subneg> result = new ArrayList<Subneg>();
-        result.add(new Subneg(this.opA_mode, this.opA, this.opB_mode, opB));
+
+        if (conditional) result.add(new Subneg(this.opA_mode, this.opA, this.opB_mode, opB, jumpToAddr));
+        else result.add(new Subneg(this.opA_mode, this.opA, this.opB_mode, opB));
         return result;
     }
 }
