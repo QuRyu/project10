77373ac1cca183ad6b17cd9349cb842804d24775
QuRyu <Quentin.Liu.0415@gmail.com>
2017-12-07 19:55:33 -0500

    rename abstract method of addressing_mode

--- src/main/java/Addressing_mode.java
+++ src/main/java/Addressing_mode.java
@@ -5,30 +5,30 @@ public enum Addressing_mode {
 
     IM {
         @Override
-        public String machine_representation() {
+        public String binary_representation() {
             return "000";
         }
     }, REGISTER {
         @Override
-        public String machine_representation() {
+        public String binary_representation() {
             return "001";
         }
     }, MEMORY {
         @Override
-        public String machine_representation() {
+        public String binary_representation() {
             return "010";
         }
     }, Input {
         @Override
-        public String machine_representation() {
+        public String binary_representation() {
             return "011";
         }
     }, Output {
         @Override
-        public String machine_representation() {
+        public String binary_representation() {
             return "100";
         }
     };
 
-    public abstract String machine_representation();
+    public abstract String binary_representation();
 }
