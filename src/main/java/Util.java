66240e6a8105fd9a06b54a47f54a8f75066b3888
QuRyu <Quentin.Liu.0415@gmail.com>
2017-12-07 21:57:26 -0500

    add Utility class

---
+++ src/main/java/Util.java
@@ -0,0 +1,22 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by Quentin on 12/6/17.
+ */
+public class Util {
+
+    public static String convertToBinary(String n) {
+        throw new NotImplementedException();
+    }
+    public static String convertToBinary(int n) {
+        throw new NotImplementedException();
+    }
+
+    public static String convertToHex(String n) {
+        throw new NotImplementedException();
+    }
+
+    public static String convertToHex(int n) {
+        throw new NotImplementedException();
+    }
+}
