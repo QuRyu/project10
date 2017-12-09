b82c628a6a03806b39f81b2c6830fed7f8ee8ca1
霍小律 <huoxiaolv@huoxiaolvdeMacBook-Air.local>
2017-12-08 23:03:15 -0500

    made util and vali

--- src/main/java/utilities/Util.java
+++ src/main/java/utilities/Util.java
@@ -11,33 +11,76 @@ public class Util {
 
     // output 8 bit long binary number
     public static String convertToBinary(String n) {
-        int num = Integer.parseInt(n);
-        String v = convertToBinary(num);
-        return v;
+        if (Integer.parseInt(n) >= -128 && Integer.parseInt(n) <= 127) {
+            int num = Integer.parseInt(n);
+            String v = convertToBinary(num);
+            return v;
+        }else{
+            throw new IllegalArgumentException("argument our of range, should be between -128 and 127");
+        }
     }
 
     public static String convertToBinary(int n) {
-        String v = "00000000";
-        if (n > 0) {
-            BigInteger num_big = new BigInteger(String.valueOf(n));
-            int l = num_big.bitLength();
-            v = v.substring(0,8-l) + Integer.toBinaryString(n);
-        }else if(n < 0){
-            int num1 = n + 128;
-            BigInteger num_big = new BigInteger(String.valueOf(num1));
-            int l = num_big.bitLength();
-            v = v.substring(0,8-l) + Integer.toBinaryString(num1);
+        if (n >= -128 && n <= 127) {
+            String v = "00000000";
+            if (n > 0) {
+
+                BigInteger num_big = new BigInteger(String.valueOf(n));
+                int l = num_big.bitLength();
+                if (n != 0) {
+                    v = v.substring(0, 8 - l) + Integer.toBinaryString(n);
+                }else{
+                    v = v.substring(0, 8 - l);
+
+                }
+            } else if (n < 0) {
+                v = "10000000";
+                int num1 = n + 128;
+                BigInteger num_big = new BigInteger(String.valueOf(num1));
+                int l = num_big.bitLength();
+                if(num1 != 0) {
+                    v = v.substring(0, 8 - l) + Integer.toBinaryString(num1);
+                }else{
+                    v = v.substring(0, 8 - l);
+
+                }
+            }
+            return v;
+        }else{
+
+            throw new IllegalArgumentException("argument our of range, should be between -128 and 127");
+        }
+    }
+//unsigned
+    public static String convertToHex(String n) {
+        if (Integer.parseInt(n) >= 0 && Integer.parseInt(n) <= 255) {
+            int num = Integer.parseInt(n);
+
+            String hex = Integer.toHexString(num);
+            if (hex.equals("0")){
+                hex = hex + "0";
+            }
+            return hex;
+
+        }else{
+            throw new IllegalArgumentException("argument our of range, should be between 0 and 255");
 
         }
-        return v;
-    }
 
-    public static String convertToHex(String n) {
-        throw new NotImplementedException();
+
     }
 
     public static String convertToHex(int n) {
-        throw new NotImplementedException();
+
+        if (n >= 0 && n <= 255) {
+            String num = String.valueOf(n);
+            String hex = convertToHex(num);
+
+            return hex;
+        }else{
+            throw new IllegalArgumentException("argument our of range, should be between 0 and 255");
+
+        }
     }
 
 }
--- src/main/java/utilities/Validation.java
+++ src/main/java/utilities/Validation.java
@@ -3,7 +3,9 @@ package utilities;
 import instruction.Instruction;
 import instruction_type.Register;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.ArrayList;
 /**
  * Created by Quentin on 12/6/17.
  * Edited by Lucia on 12/8/2017
@@ -17,21 +19,31 @@ public class Validation {
     }
 
     public static boolean validate_instruction(String ins) {
-        return Instruction.nstructions.contains(ins);
+
+        int indx = 0;
+        boolean found = false;
+        while(found == false){
+            if (ins.substring(indx,indx+1).equals(" ")){
+                found = true;
+
+            }
+            indx = indx +1;
+        }
+
+        return Instruction.nstructions.contains(ins.substring(0,indx-1));
+
     }
 
     // addr [0..255]
     public static boolean validate_memory_address(String addr) {
         int addr1 = Integer.parseInt(addr);
-        if (addr1 <= 255 || addr1 >= 0) {
+        if (addr1 <= 255 && addr1 >= 0) {
             return true;
         }else {
-            throw new NotImplementedException();
+            throw new IllegalArgumentException("argument our of range, should be between 0 and 255");
         }
     }
 
-    // addr is between the range [0..255] and has already been verified
-    private static String convertToBinary(String addr) {
-        throw new NotImplementedException();
-    }
+
+
 }
