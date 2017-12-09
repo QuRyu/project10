c91f61ed03ccae15f40eb222e5822bbffcc4d3eb
QuRyu <Quentin.Liu.0415@gmail.com>
2017-12-09 15:48:43 -0500

    corrected logical errors

--- src/main/java/utilities/Validation.java
+++ src/main/java/utilities/Validation.java
@@ -15,7 +15,18 @@ public class Validation {
     }
 
     public static boolean validate_register(String opA, String opB) {
-        return Register.register_lookup(opA) || Register.register_lookup(opB);
+        return Register.register_lookup(opA) && Register.register_lookup(opB);
+    }
+
+    // check if the register is valid; throw exception if not
+    public static void validate_register_exception(String regis) {
+        if (!validate_register(regis))
+            throw new IllegalArgumentException("Register " + regis + " is not valid");
+    }
+
+    public static void validate_register_exception(String opA, String opB) {
+        if (!validate_register(opA, opB))
+            throw new IllegalArgumentException("Register " + (validate_register(opA) ? opB : opA) + " is not valid");
     }
 
     public static boolean validate_instruction(String ins) {
@@ -25,11 +36,9 @@ public class Validation {
     // addr [0..255]
     public static boolean validate_memory_address(String addr) {
         int addr1 = Integer.parseInt(addr);
-        if (addr1 <= 255 || addr1 >= 0) {
-            return true;
-        }else {
-            throw new NotImplementedException();
-        }
+        if (addr1 <= 255 && addr1 >= 0) return true;
+        else throw new IllegalArgumentException("memory address " + addr + " is out of boundary [0..255]");
+
     }
 
     // addr is between the range [0..255] and has already been verified
